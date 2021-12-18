1e2f7dda9fd6d9cb4ae5109fa2dcd7d99630db07
TomatoBigBig <a17504190204@163.com>
2021-12-18 22:56:16 +0800

    gengxin

--- webhook.js
+++ webhook.js
@@ -1,5 +1,5 @@
 let http = require('http');
-let server = http.createServer((req, res) => {
+let server = http.createServer(function(req, res){
     console.log(req.method, req.url)
     if(req.method == 'POST' && req.url == '/webhook'){
         res.setHeader('Content-Type','application/json')
