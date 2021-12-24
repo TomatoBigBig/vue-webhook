78c068261ef3f4bc73e34badb4beace80bd6b275
TomatoBigBig <a17504190204@163.com>
2021-12-25 03:32:28 +0800

    gx

--- webhook.js
+++ webhook.js
@@ -1,13 +1,34 @@
 let http = require('http');
-let server = http.createServer(function(req, res){
+let createHandler = require('github-webhook-handler')
+let handler = createHandler({path:'/webhook', secret:'123456'})
+
+
+http.createServer(function(req, res){
     console.log(req.method, req.url)
-    if(req.method == 'POST' && req.url == '/webhook'){
-        res.setHeader('Content-Type','application/json')
-        res.end(JSON.stringify({ok:true}))
-    }else {
-        res.end('啥也不是')
-    }
-})
-server.listen(8099,() => {
+    handler(res,req,function(){
+        res.statusCode = 404
+        res.end('not found')
+    })
+    // if(req.method == 'POST' && req.url == '/webhook'){
+    //     res.setHeader('Content-Type','application/json')
+    //     res.end(JSON.stringify({ok:true}))
+    // }else {
+    //     res.end('啥也不是')
+    // }
+}).listen(8099,() => {
     console.log('webhook start')
+})
+
+
+handler.on('error', function(err){
+    console.log('error:',err.message)
+})
+
+handler.on('push', function(event){
+    console.log('Listen to the push')
+})
+
+
+handler.on('issues', function(event){
+    console.log('Listen to the issues')
 })
