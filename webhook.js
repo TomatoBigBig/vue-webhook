b61620d76d8f2151aa3d27d2ccfcbda29bedcb81
TomatoBigBig <a17504190204@163.com>
2022-01-03 18:01:31 +0800

    gx

--- webhook.js
+++ webhook.js
@@ -1,21 +1,43 @@
 
+// let http = require('http')
+// let server = http.createServer((req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin','*')
+//     // if(req.url === '/api/users'){
+//     //     res.end(JSON.stringify(users))
+//     // }else{
+//     //     res.end('aiwoqu 求你了！！！！！')
+//     // }
+//     console.log('receive webhook request')
+//     if(req.url === '/api/webhook'){
+//         // res.setHeader('Content-Type','application/json')
+//         console.log('return is ok')
+//         res.end(JSON.stringify({ok:true}))
+//     }else {
+//         res.end('啥也不是')
+//     }
+// });
+// server.listen(3002,() => {
+//     console.log('start webhook giaogiaogiao')
+// })
 let http = require('http')
+let users = [
+    {
+        id:111,
+        name:'aiwocao'
+    },
+    {
+        id:222,
+        name:'wonima'
+    }
+]
 let server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin','*')
-    // if(req.url === '/api/users'){
-    //     res.end(JSON.stringify(users))
-    // }else{
-    //     res.end('aiwoqu 求你了！！！！！')
-    // }
-    console.log('receive webhook request')
-    if(req.url === '/api/webhook'){
-        // res.setHeader('Content-Type','application/json')
-        console.log('return is ok')
-        res.end(JSON.stringify({ok:true}))
-    }else {
-        res.end('啥也不是')
+    if(req.url === '/api/ws'){
+        res.end(JSON.stringify(users))
+    }else{
+        res.end('aiwoqu 求你了！！！！！')
     }
 });
 server.listen(3002,() => {
-    console.log('start webhook giaogiaogiao')
+    console.log('start service')
 })
