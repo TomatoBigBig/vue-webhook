c5bf10c919142c11d2ee8b0d4ffa35917b37c9da
TomatoBigBig <a17504190204@163.com>
2022-01-03 17:40:04 +0800

    gx

--- webhook.js
+++ webhook.js
@@ -8,7 +8,7 @@ let server = http.createServer((req, res) => {
     //     res.end('aiwoqu 求你了！！！！！')
     // }
     console.log('receive webhook request')
-    if(req.url === '/webhook/receive'){
+    if(req.url === '/api/webhook'){
         // res.setHeader('Content-Type','application/json')
         console.log('return is ok')
         res.end(JSON.stringify({ok:true}))
@@ -17,5 +17,5 @@ let server = http.createServer((req, res) => {
     }
 });
 server.listen(3002,() => {
-    console.log('start webhook gogogo')
+    console.log('start webhook giaogiaogiao')
 })
