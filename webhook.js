c4c447db34d7d5546930eddef5064881026ae14b
TomatoBigBig <a17504190204@163.com>
2022-01-03 16:51:29 +0800

    更新 修改端口

--- webhook.js
+++ webhook.js
@@ -1,34 +1,19 @@
-let http = require('http');
-let createHandler = require('github-webhook-handler')
-let handler = createHandler({path:'/webhook', secret:'123456'})
 
-
-http.createServer(function(req, res){
-    console.log(req.method, req.url)
-    handler(res,req,function(){
-        res.statusCode = 404
-        res.end('not found')
-    })
-    // if(req.method == 'POST' && req.url == '/webhook'){
-    //     res.setHeader('Content-Type','application/json')
-    //     res.end(JSON.stringify({ok:true}))
-    // }else {
-    //     res.end('啥也不是')
+let http = require('http')
+let server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    // if(req.url === '/api/users'){
+    //     res.end(JSON.stringify(users))
+    // }else{
+    //     res.end('aiwoqu 求你了！！！！！')
     // }
-}).listen(8099,() => {
-    console.log('webhook start gogogo')
-})
-
-
-handler.on('error', function(err){
-    console.log('error:',err.message)
-})
-
-handler.on('push', function(event){
-    console.log('Listen to the push')
-})
-
-
-handler.on('issues', function(event){
-    console.log('Listen to the issues')
+    if(req.url === '/webhook'){
+        // res.setHeader('Content-Type','application/json')
+        res.end(JSON.stringify({ok:true}))
+    }else {
+        res.end('啥也不是')
+    }
+});
+server.listen(3002,() => {
+    console.log('start webhook gogogo')
 })
